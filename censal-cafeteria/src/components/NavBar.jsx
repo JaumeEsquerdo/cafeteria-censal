9de07448f04639782c4b7e0685460ba4b9fed27f
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-03-30 12:17:14 +0200

    cambiar shadow de los "botones"

--- censal-cafeteria/src/components/NavBar.jsx
+++ censal-cafeteria/src/components/NavBar.jsx
@@ -2,13 +2,13 @@ export const NavBar = ({ navigation, setNavigation }) => {
   return (
     <ul className="flex gap-4">
       <li
-        className={`${navigation === "comidas" ? "bg-blue-300" : ""} text-xl py-2 px-4 bg-blue-100 rounded-4xl border-2 cursor-pointer shadow-lg transition-colors duration-200`}
+        className={`${navigation === "comidas" ? "bg-blue-300" : ""} text-xl py-2 px-4 bg-blue-100 rounded-4xl border-2 cursor-pointer shadow-md transition-colors duration-200`}
         onClick={() => setNavigation("comidas")}
       >
         Comidas
       </li>
       <li
-        className={`${navigation === "bebidas" ? "bg-blue-300" : ""}  text-xl py-2 px-4 bg-blue-100 rounded-4xl border-2 cursor-pointer shadow-lg  transition.colors duration-200`}
+        className={`${navigation === "bebidas" ? "bg-blue-300" : ""}  text-xl py-2 px-4 bg-blue-100 rounded-4xl border-2 cursor-pointer shadow-md  transition.colors duration-200`}
         onClick={() => setNavigation("bebidas")}
       >
         Bebidas
