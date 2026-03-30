f78b0e0a4c2e5e39e552372a73edc436cdab9422
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-03-30 12:15:19 +0200

    layout general de la carta/web

---
+++ censal-cafeteria/src/App.jsx
@@ -0,0 +1,53 @@
+import { NavBar } from "./components/NavBar";
+import { useState } from "react";
+
+function App() {
+  const [navigation, setNavigation] = useState("comidas");
+  return (
+    <>
+      <div className="text-4xl flex flex-col gap-6 min-h-screen bg-blue-50  max-w-full ">
+        <header className="flex flex-col gap-4 pt-4 items-center">
+          <div className="flex flex-col gap-[0.4px]">
+            <h1 className="text-center text-2xl">la carta de la</h1>
+            <h2 className="text-3xl font-medium"> Cafetería Censal</h2>
+          </div>
+          <NavBar navigation={navigation} setNavigation={setNavigation} />
+        </header>
+        <main>
+          {navigation === "comidas" && (
+            <div className="flex flex-col gap-6 p-4 pb-6">
+              <img
+                src="/imgs/brava-pago.png"
+                alt="Carta de comidas"
+                className="rounded-2xl shadow-xl w-full max-w-full h-auto block"
+              />
+              <img
+                src="/imgs/brava-pago.png"
+                alt="Carta de comidas"
+                className="rounded-2xl shadow-xl w-full max-w-full h-auto block"
+              />
+            </div>
+          )}
+          {navigation === "bebidas" && (
+            <div className="flex flex-col pt-4 pb-6 gap-6 overflow-x-auto">
+              <div className="w-200  mx-4 shadow-xl">
+                <img
+                  src="/imgs/hotel-fachada.webp"
+                  className="w-full h-full object-cover rounded-xl"
+                />
+              </div>
+              <div className="w-200    mx-4 shadow-xl">
+                <img
+                  src="/imgs/hotel-fachada.webp"
+                  className="w-full h-full object-cover rounded-xl"
+                />
+              </div>
+            </div>
+          )}
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
