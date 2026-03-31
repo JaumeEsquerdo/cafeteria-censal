e6615be33b50c4fb60dfecb70a25a3d12cb48287
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-03-31 12:14:07 +0200

    cambiar colores btns nav por los de la carta

--- censal-cafeteria/src/components/LiquidNav.jsx
+++ censal-cafeteria/src/components/LiquidNav.jsx
@@ -14,13 +14,13 @@ export const LiquidNav = ({ setNavigation }) => {
             setActiveTab(tab);
             setNavigation(tab);
           }}
-          className={`relative text-xl font-medium py-2 px-4 bg-blue-200 rounded-4xl border-2 cursor-pointer shadow-md overflow-hidden ${activeTab === tab ? "text-black" : "text-gray-600 border-gray-400"}  active:scale-[0.7]  transition-all duration-800 ease-in-out`}
+          className={`relative text-xl font-medium py-2 px-4 bg-neutral-100 rounded-4xl border-2 cursor-pointer shadow-md overflow-hidden ${activeTab === tab ? "text-black" : "text-gray-600 border-gray-400"}  active:scale-[0.7]  transition-all duration-800 ease-in-out`}
         >
           <span className="relative z-10">{tab}</span>
           {activeTab === tab && (
             <motion.div
               layoutId="bubble"
-              className="absolute inset-0 bg-blue-300 z-0 rounded-2xl"
+              className="absolute inset-0 z-0 rounded-2xl bg-[#f2eeeb]"
               transition={{ type: "spring", bounce: 0.3, duration: 1.8 }}
             />
           )}
